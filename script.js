d1f72de7b89c1b1ede66be96c9921a5c983362da
aftabislam24 <158824071+aftabislam24@users.noreply.github.com>
2025-05-06 04:23:18 +0000

    done

---
+++ script.js
@@ -0,0 +1,24 @@
+let lists = document.querySelectorAll(`.list`);
+let leftBox = document.querySelector(`.left`);
+let rightBox = document.querySelector(`.right`);
+
+for(item of lists){
+    item.addEventListener(`dragstart`,(e)=>{
+        let selected = e.target;
+
+        rightBox.addEventListener(`dragover`,(e)=>{
+            e.preventDefault();
+        });
+        rightBox.addEventListener(`drop`,(e)=>{
+            rightBox.appendChild(selected);
+            selected= null;
+        })
+        leftBox.addEventListener(`dragover`,(e)=>{
+            e.preventDefault();
+        })
+        leftBox.addEventListener(`drop`,(e)=>{
+            leftBox.appendChild(selected);
+            selected= null;
+        })
+    })
+}
